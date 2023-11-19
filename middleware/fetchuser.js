a764a9b6ff089fc0573ce4fa39b50a35d50bd870
vky-fsd <vk206vignesh@gmail.com>
2023-11-20 02:21:05 +0530

    chnages

--- middleware/fetchuser.js
+++ middleware/fetchuser.js
@@ -1,18 +1,9 @@
 const jwt = require("jsonwebtoken");
-
 const fetchuser = (req, res, next) => {
-  const authorizationHeader = req.headers.authorization;
-
-  if (!authorizationHeader) {
-    return res.status(401).send({ error: "Please authenticate using a valid token" });
+  const token = req.header("auth-token");
+  if (!token) {
+    res.status(401).send({ error: "Please authenticate using a valid token" });
   }
-
-  const [bearer, token] = authorizationHeader.split(" ");
-
-  if (bearer !== "Bearer" || !token) {
-    return res.status(401).send({ error: "Invalid Authorization header format" });
-  }
-
   try {
     const data = jwt.verify(token, process.env.REACT_APP_JWT_SECRET);
     req.user = data.user;
@@ -20,6 +11,7 @@ const fetchuser = (req, res, next) => {
   } catch (error) {
     res.status(401).send({ error: "Please authenticate using a valid token" });
   }
-};
+
+}
 
 module.exports = fetchuser;
