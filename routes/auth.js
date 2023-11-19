5325b6af9ab4138d515388923c193339fef11858
vky-fsd <vk206vignesh@gmail.com>
2023-11-19 21:06:23 +0530

    error handling updated

--- routes/auth.js
+++ routes/auth.js
@@ -7,46 +7,46 @@ const fetchuser = require("../middleware/fetchuser");
 const router = express.Router();
 // ROUTE 1: Create a user
 // POST '/api/auth/createuser'
-router.post(
-  "/createuser",
-  [
-    body("name", "Enter a valid name").exists(),
-    body("email", "Enter a valid email").isEmail(),
-    body("password", "Enter a valid password").exists(),
-  ],
-  async (req, res) => {
-    const errors = validationResult(req);
-    try {
-      if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-      }
-      let user = await User.findOne({ email: req.body.email });
-      if (user) {
-        return res
-          .status(400)
-          .json({ error: "User with this email already exists!" }); // Fix the typo here
-      }
-      const salt = await bcrypt.genSalt(10);
-      const secPass = await bcrypt.hash(req.body.password, salt);
-      user = await User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: secPass,
-      });
 
-      const data = {
-        user: {
-          id: user.id,
-        },
-      };
-      const authtoken = jwt.sign(data, process.env.REACT_APP_JWT_SECRET);
-      res.json({ success: true, authtoken });
-    } catch (error) {
-      console.error(error.message);
-      res.status(500).send("Internal Server Error");
+
+router.post("/createuser", [
+  body("name", "Enter a valid name").exists(),
+  body("email", "Enter a valid email").isEmail(),
+  body("password", "Enter a valid password").exists(),
+], async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
+
+    let user = await User.findOne({ email: req.body.email });
+    if (user) {
+      return res.status(400).json({ error: "User with this email already exists!" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const secPass = await bcrypt.hash(req.body.password, salt);
+
+    user = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: secPass,
+    });
+
+    const data = {
+      user: {
+        id: user.id,
+      },
+    };
+    const authtoken = jwt.sign(data, process.env.REACT_APP_JWT_SECRET);
+    res.json({ success: true, authtoken });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: "Internal Server Error" });
   }
-);
+});
+
 
 
 // ROUTE 2: Authenticate a user
